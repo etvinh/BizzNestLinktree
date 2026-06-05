495f41760e1cce5c80617a3f75e3a11abede7936
etvinh <eth.vinh@gmail.com>
2026-06-04 19:04:37 -0700

    add name test

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,7 +1,12 @@
-import { it } from 'vitest'
-import { render } from '@testing-library/react'
+import { it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
 import App from '../src/App.jsx'
 
 it('renders without crashing', () => {
   render(<App />)
+})
+
+it('renders Ethan Vinh', () => {
+  render(<App />)
+  expect(screen.getByText('Ethan Vinh')).toBeInTheDocument()
 })
